4bee52ff350bc27e03643c5d0d59b4047d99f6c9
Horus Solutions <36744399+HorusInfo@users.noreply.github.com>
2025-09-26 17:11:43 -0300

    Web app

--- gestor/gestor/wsgi.py
+++ gestor/gestor/wsgi.py
@@ -11,7 +11,7 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'gestor/gestor.settings')
-
-app = get_wsgi_application()
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'gestor.settings')
+application = get_wsgi_application()
+app = application
 
