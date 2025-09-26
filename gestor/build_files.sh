1f934a697656df2659efc71ed1a0564b2e9958d1
Horus Solutions <36744399+HorusInfo@users.noreply.github.com>
2025-09-26 14:49:07 -0300

    Web app

--- gestor/build_files.sh
+++ gestor/build_files.sh
@@ -1,3 +1,3 @@
-python gestor/manage.py collectstatic --noinput
-python -m pip install -r requirements.txt
-python manage.py collectstatic --noinput
+python3 gestor/manage.py collectstatic --noinput
+python3 -m pip install -r requirements.txt
+python3 manage.py collectstatic --noinput
